f5daeed9e2139b37cd1ba1d2cbbc79aa59a353f7
ADMIN <ADMIN@DESKTOP-CO17EJL>
2025-09-19 21:48:28 +0530

    " All DataTypes"

---
+++ src/com/tns/Datatypes/DataTypesDemo.java
@@ -0,0 +1,36 @@
+package com.tns.Datatypes;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		
+		// Integer data type
+		int age = 20;
+		
+		// String
+		String name = "Sindhuja";
+		
+		// Character data type
+		char grade = 'A';
+		
+		// Double data type
+		double salary = 50000.600;
+		
+		// Boolean data type
+		boolean isStudent = true;
+		
+		System.out.println("Name:"+ name);
+		
+		System.out.println("Age:"+ age);
+		
+		System.out.println("Grade:"+ grade);
+		
+		System.out.println("Salary:"+ salary);
+		
+		System.out.println("Is Student:"+ isStudent);
+		
+		
+
+	}
+
+}
