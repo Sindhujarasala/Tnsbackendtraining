e6ec842be94bc8cea866d25aa79e9025393a3108
ADMIN <ADMIN@DESKTOP-CO17EJL>
2025-09-19 22:22:17 +0530

    "Types of Variables"

---
+++ src/com/tns/Variables/VariablesDemo.java
@@ -0,0 +1,26 @@
+package com.tns.Variables;
+
+// Demo for Types of Variables
+
+public class VariablesDemo {
+	
+	// Instance Variable
+	
+	String name = " This is Instance Variable ";
+	
+	// Static Variable
+	
+	static String namethree = " This is Static Variable ";
+	
+	// Local Variable
+	public void print() {
+	
+	String local = " This is Local Variable ";
+	System.out.println(local);
+	
+	
+	
+	}
+	
+
+}
---
+++ src/com/tns/Variables/VariablesDemoMain.java
@@ -0,0 +1,15 @@
+package com.tns.Variables;
+
+public class VariablesDemoMain {
+
+	public static void main(String[] args) {
+		
+		VariablesDemo ob = new VariablesDemo();
+		System.out.println(ob.name);
+		ob. print();
+		System.out.println(VariablesDemo.namethree);
+		
+		
+	}
+
+}
