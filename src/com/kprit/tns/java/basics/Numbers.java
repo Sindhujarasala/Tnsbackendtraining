3d8175ff1acced1041d88aa0bd74414835ffad13
ADMIN <ADMIN@DESKTOP-CO17EJL>
2025-09-19 15:40:05 +0530

    "Numbers"

---
+++ src/com/kprit/tns/java/basics/Numbers.java
@@ -0,0 +1,19 @@
+package com.kprit.tns.java.basics;
+
+public class Numbers {
+	
+	// Method for Numbers
+	
+	public void numbers() {
+		
+		for(int i=1; i<=5; i++) {
+			System.out.println(i);
+			
+			
+		}
+	}
+	
+	
+	
+
+}
---
+++ src/com/kprit/tns/java/basics/NumbersMain.java
@@ -0,0 +1,14 @@
+package com.kprit.tns.java.basics;
+
+public class NumbersMain {
+
+	public static void main(String[] args) {
+		// Object for Numbers Class
+		
+		Numbers Ob = new Numbers();
+		Ob.numbers();
+		
+
+	}
+
+}
