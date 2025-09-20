a9157ac15852c7137501ed3b4b217ab9a42c50e8
Sindhujarasala <sindhujarasala2094@gmail.com>
2025-09-20 21:31:13 +0530

    "If-Else Statement"

---
+++ src/com/tns/ControlStatementsDemo/IfElseDemo.java
@@ -0,0 +1,24 @@
+package com.tns.ControlStatementsDemo;
+   // Program for IfElse Statement
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		
+		        int age = 11;
+		        if (age >= 18)
+		            System.out.println(" You are eligible for Voting");
+		        
+		        else
+		            System.out.println("Sorry! You are not eligible");
+		        
+		        System.out.println("if else statement");
+	
+		        }
+		    
+		
+		
+
+	}
+
+
