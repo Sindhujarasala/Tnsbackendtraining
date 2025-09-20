e54cacaea1290e59eaf203e59391aa5f4245199a
Sindhujarasala <sindhujarasala2094@gmail.com>
2025-09-20 22:09:25 +0530

    "While loop"

---
+++ src/com/tns/LoopsDemo/WhileLoopDemo.java
@@ -0,0 +1,19 @@
+package com.tns.LoopsDemo;
+
+    // Program for While loop
+    //when we do not know how many iterations it should execute
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		int i=99;
+        while(i<=100)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+
+	}
+
+
