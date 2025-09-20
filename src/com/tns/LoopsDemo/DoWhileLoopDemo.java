f1b4732d8d7924119076b85937c2fef7d4f00a5f
Sindhujarasala <sindhujarasala2094@gmail.com>
2025-09-20 22:14:30 +0530

    "Dowhile loop"

---
+++ src/com/tns/LoopsDemo/DoWhileLoopDemo.java
@@ -0,0 +1,19 @@
+package com.tns.LoopsDemo;
+
+    // Program for Dowhile loop
+    // First execute later check with condition
+
+public class DoWhileLoopDemo {
+
+	public static void main(String[] args) {
+		int i = 6749;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 1);
+    }
+
+
+	}
+
+
