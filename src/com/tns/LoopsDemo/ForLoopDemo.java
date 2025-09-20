169f05101487c7e044649850a634a2813e687d95
Sindhujarasala <sindhujarasala2094@gmail.com>
2025-09-20 21:55:18 +0530

    "For loop"

---
+++ src/com/tns/LoopsDemo/ForLoopDemo.java
@@ -0,0 +1,19 @@
+package com.tns.LoopsDemo;
+
+    // Program for Forloop
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		// Using forloop
+		
+		for(int i = 1; i<=100; i++) //(int i = 100; i<=1;i--)
+        {
+            System.out.print("Value of i:");
+            System.out.println(i);
+        }
+    }
+
+
+	}
+
+
