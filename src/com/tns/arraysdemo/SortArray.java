ccb7a29ae87e6a18bf4a54029ff6de9e2a056b60
Sindhujarasala <sindhujarasala2094@gmail.com>
2025-09-21 12:37:27 +0530

    "Sort Array"

---
+++ src/com/tns/arraysdemo/SortArray.java
@@ -0,0 +1,21 @@
+package com.tns.arraysdemo;
+
+import java.util.Arrays;
+
+// Program for sorting an array
+public class SortArray {
+
+	public static void main(String[] args) {
+		
+		        int[] arr = {5, 2, 8, 7, 1};
+		        Arrays.sort(arr);
+		        System.out.println("Sorted: " + Arrays.toString(arr));
+		    }
+		
+
+		
+		
+
+	}
+
+
