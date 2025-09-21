47a51b01c80f179428002c4523ed495f3fb74d73
Sindhujarasala <sindhujarasala2094@gmail.com>
2025-09-21 12:05:44 +0530

    "Basic Array"

---
+++ src/com/tns/arraysdemo/BasicArray.java
@@ -0,0 +1,26 @@
+package com.tns.arraysdemo;
+
+     // Program for Basic Array
+public class BasicArray {
+
+	public static void main(String[] args) {
+	
+		        int[] arr;
+		        arr = new int[5]; // size of the array
+
+		        arr[0] = 10;
+		        arr[1] = 20;
+		        arr[2] = 30;
+		        arr[3] = 40;
+		        arr[4] = 50;
+
+		        for (int i = 0; i < arr.length; i++) {
+		            System.out.println("Element at index " + i + " is " + arr[i]);
+		        }
+		    }
+		
+		
+
+	}
+
+
