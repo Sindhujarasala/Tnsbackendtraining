3fed314da26f40b69c20e8bbbf124ef3c4a899b1
Sindhujarasala <sindhujarasala2094@gmail.com>
2025-09-21 12:23:45 +0530

    "Single Array"

---
+++ src/com/tns/arraysdemo/SingleArray.java
@@ -0,0 +1,21 @@
+package com.tns.arraysdemo;
+
+public class SingleArray {
+
+	public static void main(String[] args) {
+		// Single one dimensional array - Sum of elements in an array
+		
+		        int arr[] = {1, 2, 3, 4};
+		        int sum = 0; // Corrected initialization
+		        for (int i = 0; i < arr.length; i++) { // Corrected loop variable type
+		            sum += arr[i];
+		        }
+		        System.out.println("sum of array is: " + sum); // Corrected System.out.println
+		    }
+
+		    
+		}
+
+	
+
+
